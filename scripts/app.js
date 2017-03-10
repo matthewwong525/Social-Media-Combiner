1815d909d5640f664e1b84e9c507ed72a64cea99
Matthew Wong <Matthew Wong>
2017-03-09 22:33:07 -0500

    added angular

--- scripts/app.js
+++ scripts/app.js
@@ -8,26 +8,68 @@ var config = {
     messagingSenderId: "808569769768"
 };
 firebase.initializeApp(config);
-
 //Retrieve Firebase Messaging object
 const messaging = firebase.messaging();
 
-messaging.requestPermission().then(function(){
-    console.log('Notification permission granted.');
-    messaging.getToken();
-})
+(function(){
+    
+    var app = angular.module('website',[])
 
-.catch(function(err){
-    console.log('Unable to get permission to notify', err);
-});
+    app.config(['$interpolateProvider', function($interpolateProvider) {
+      $interpolateProvider.startSymbol('{/');
+      $interpolateProvider.endSymbol('/}');
+    }]);
 
-messaging.getToken().then(function(currentToken){
-    if(currentToken){
-        console.log(currentToken);
-    }else{
-        console.log("No Instance ID token avialable");
+    app.controller("WebsiteController",function(){
+        console.log("hello world");
+        this.loggedIn = function(isLoggedIn){
+            alert("hi");
+        };
+        
+    });
+
+})();
+
+function requestPermission(){
+    messaging.requestPermission().then(function(){
+        console.log('Notification permission granted.');
+        getToken();
+    }).catch(function(err){
+        console.log('Unable to get permission to notify', err);
+    });
+}
+
+function getToken(){
+    messaging.getToken().then(function(currentToken){
+        if(currentToken){
+            console.log(currentToken);
+            //TODO: send token to the server
+        }else{
+            console.log("No Instance ID token avialable");
+        }
+    }).catch(function(err){
+        console.log("Error occured while retrieving token",err);
+    });
+}
+
+
+function isTokenSentToServer() {
+    if (window.localStorage.getItem('sentToServer') == 1) {
+          return true;
     }
-})
-.catch(function(err){
-    console.log("Error occured while retrieving token",err);
-});
+    return false;
+  }
+
+function setTokenSentToServer(sent) {
+    window.localStorage.setItem('sentToServer', sent ? 1 : 0);
+}
+
+function sendTokenToServer(currentToken){
+    if(!isTokenSentToServer()){
+        console.log("Sending token to server...");
+        //TODO Send the current token to server
+        setTokenSentToServer(true);
+    }else{
+        console.log("Token already sent to server");
+    }
+}
