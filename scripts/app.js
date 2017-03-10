7ca5f3701a598f61d870d5c2fe1af3a9050e3f68
Matthew Wong <mw2017@dhltd.corp>
2017-03-10 15:33:20 -0500

    Added services and fixed issue with multiple calls on a function in html

--- scripts/app.js
+++ scripts/app.js
@@ -14,62 +14,66 @@ const messaging = firebase.messaging();
 (function(){
     
     var app = angular.module('website',[])
-
+    
     app.config(['$interpolateProvider', function($interpolateProvider) {
       $interpolateProvider.startSymbol('{/');
       $interpolateProvider.endSymbol('/}');
     }]);
 
-    app.controller("WebsiteController",function(){
+    app.controller("WebsiteController", function (FirebaseService) {
         console.log("hello world");
-        this.loggedIn = function(isLoggedIn){
-            alert("hi");
+        this.loggedIn = function (isLoggedIn) {
+            FirebaseService.requestPermission(isLoggedIn);
         };
         
     });
 
-})();
-
-function requestPermission(){
-    messaging.requestPermission().then(function(){
-        console.log('Notification permission granted.');
-        getToken();
-    }).catch(function(err){
-        console.log('Unable to get permission to notify', err);
-    });
-}
-
-function getToken(){
-    messaging.getToken().then(function(currentToken){
-        if(currentToken){
-            console.log(currentToken);
-            //TODO: send token to the server
-        }else{
-            console.log("No Instance ID token avialable");
+    app.service('FirebaseService', function () {
+        var getToken = function () {
+            messaging.getToken().then(function (currentToken) {
+                if (currentToken) {
+                    console.log(currentToken);
+                    //TODO: send token to the server
+                } else {
+                    console.log("No Instance ID token avialable");
+                }
+            }).catch(function (err) {
+                console.log("Error occured while retrieving token", err);
+            });
         }
-    }).catch(function(err){
-        console.log("Error occured while retrieving token",err);
+
+        this.requestPermission = function (isLoggedIn) {
+            if (isLoggedIn) {
+                messaging.requestPermission().then(function () {
+                    console.log('Notification permission granted.');
+                    getToken();
+                }).catch(function (err) {
+                    console.log('Unable to get permission to notify', err);
+                });
+            }
+        }
+        
     });
-}
 
-
-function isTokenSentToServer() {
-    if (window.localStorage.getItem('sentToServer') == 1) {
-          return true;
+    //TODO: use services, factories, modules and dependencies
+    function isTokenSentToServer() {
+        if (window.localStorage.getItem('sentToServer') == 1) {
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-function setTokenSentToServer(sent) {
-    window.localStorage.setItem('sentToServer', sent ? 1 : 0);
-}
-
-function sendTokenToServer(currentToken){
-    if(!isTokenSentToServer()){
-        console.log("Sending token to server...");
-        //TODO Send the current token to server
-        setTokenSentToServer(true);
-    }else{
-        console.log("Token already sent to server");
+    function setTokenSentToServer(sent) {
+        window.localStorage.setItem('sentToServer', sent ? 1 : 0);
     }
-}
+
+    function sendTokenToServer(currentToken) {
+        if (!isTokenSentToServer()) {
+            console.log("Sending token to server...");
+            //TODO Send the current token to server
+            setTokenSentToServer(true);
+        } else {
+            console.log("Token already sent to server");
+        }
+    }
+})();
