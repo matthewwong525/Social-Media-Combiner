7d9715d915a7836f54a6d2d1081ac7f141479ef5
Matthew Wong <mw2017@dhltd.corp>
2017-03-29 15:29:17 -0400

    added client side escaping

--- scripts/messageservice.js
+++ scripts/messageservice.js
@@ -1,4 +1,20 @@
 (function(){
+    var entityMap = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#39;',
+      '/': '&#x2F;',
+      '`': '&#x60;',
+      '=': '&#x3D;'
+    };
+
+    function escapeHtml (string) {
+      return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+        return entityMap[s];
+      });
+    }
     var app = angular.module('message-service',['token-service','update-service'])
     app.service('MessageService', ['$http', 'TokenService','UpdateService','$firebaseAuth', function ($http,TokenService,UpdateService,$firebaseAuth) {
         var userToSend = "";
@@ -9,7 +25,8 @@
                 var message = payload.data.message.replace("\n", "&#xA;");
                 var d = new Date();
                 var date= d.getMonth()+"/"+d.getDate()+"/"+d.getFullYear() + " " +d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
-                $("#incomingText").append(date+" *" + payload.data.username + "* :" + "&#xA;" + message + "&#xA;");
+                //TODO: CHANGE CURRENT USER TO DISPLAYNAME OR EMAIL
+                $("#incomingText").append(date+" *" + escapeHtml(payload.data.username) + "* :" + "&#xA;" + escapeHtml(message)  + "&#xA;");
 
             }else{ //send the notification to the user
                 //TODO: ALSO MAKE NOTIFICATIONS FROM BACKGROUND MESSAGES
@@ -27,7 +44,8 @@
             var parameters = JSON.stringify({ sendUser: TokenService.getCurrentUser(), receiveUser:username, message: message });
             var d = new Date();
             var date= d.getMonth()+1+"/"+d.getDate()+"/"+d.getFullYear() + " " +d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
-            $("#incomingText").append(date+" *" + TokenService.getCurrentUser() + "* :" + "&#xA;" + message + "&#xA;");
+            //TODO: CHANGE CURRENT USER TO DISPLAYNAME OR EMAIL 
+            $("#incomingText").append(date+" *" + TokenService.getCurrentUser() + "* :" + "&#xA;" + escapeHtml(message) + "&#xA;");
             $http.post("/sendMessageToUser/",parameters)
                 .then(function(response){
                     //sets the notifications for the person who is being sent the message
